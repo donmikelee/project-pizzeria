156d3dd210ee1dc51b7b5a1a103c1f9810a5ff66
Michał Górny <michalgorny94@gmail.com>
2020-11-10 17:12:37 +0100

    Add extra files

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,10 +4,13 @@ export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product', // CODE ADDED
+    bookingWidget: '#template-booking-widget',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -29,6 +32,15 @@ export const select = {
       linkIncrease: 'a[href="#more"]',
     },
   },
+  datePicker: {
+    wrapper: '.date-picker',
+    input: `input[name="date"]`,
+  },
+  hourPicker: {
+    wrapper: '.hour-picker',
+    input: 'input[type="range"]',
+    output: '.output',
+  },
   // CODE ADDED START
   cart: {
     productList: '.cart__order-summary',
@@ -41,6 +53,14 @@ export const select = {
     formSubmit: '.cart__order [type="submit"]',
     phone: '[name="phone"]',
     address: '[name="address"]',
+  },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: {
+    links: '.main-nav a',
   },
   cartProduct: {
     amountWidget: '.widget-amount',
@@ -60,24 +80,49 @@ export const classNames = {
   cart: {
     wrapperActive: 'active',
   },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
   // CODE ADDED END
 };
   
 export const settings = {
+  hours: {
+    open: 12,
+    close: 24,
+  },
   amountWidget: {
     defaultValue: 1,
     defaultMin: 1,
     defaultMax: 9,
-  }, // CODE CHANGED
-  // CODE ADDED START
+  },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
   cart: {
     defaultDeliveryFee: 5,
+  },
+  booking: {
+    tableIdAttribute: 'data-table',
   },
   // CODE ADDED END
   db: {
     url: '//localhost:3131',
     product: 'product',
     order: 'order',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
   }
 };
   
@@ -86,4 +131,5 @@ export const templates = {
   // CODE ADDED START
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   // CODE ADDED END
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
 };
