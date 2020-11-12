ed0b370e302fe67bb72136224bfd694634b8ce2e
Michał Górny <michalgorny94@gmail.com>
2020-11-12 18:05:41 +0100

    Add two subpages

--- src/js/app.js
+++ src/js/app.js
@@ -58,19 +58,19 @@ const app = {
     thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
     thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
 
-    thisApp.activatePage(thisApp.pages[0].id);
+    // thisApp.activatePage(thisApp.pages[0].id);
 
-    // let pagesMatchingHash = [];
+    let pagesMatchingHash = [];
 
-    // if(window.location.hash.length > 2){
-    //   const idFromHash = window.location.hash.replace('#/', '');
+    if(window.location.hash.length > 2){
+      const idFromHash = window.location.hash.replace('#/', '');
 
-    //   pagesMatchingHash = thisApp.pages.filter(function(page){
-    //     return page.id == idFromHash;
-    //   });
-    // }
+      pagesMatchingHash = thisApp.pages.filter(function(page){
+        return page.id == idFromHash;
+      });
+    }
 
-    // thisApp.activatePage(pagesMatchingHash.length ? pagesMatchingHash[0].id : thisApp.pages[0].id);
+    thisApp.activatePage(pagesMatchingHash.length ? pagesMatchingHash[0].id : thisApp.pages[0].id);
 
     for (let link of thisApp.navLinks){
       link.addEventListener('click', function(event){
@@ -79,11 +79,11 @@ const app = {
 
         /* TODO: get page id from href */
 
-        clickedElement.getAttribute('href').replace('#','');
+        const pageId = clickedElement.getAttribute('href').replace('#','');
 
         /* TODO: activate page */
 
-        thisApp.activatePage();
+        thisApp.activatePage(pageId);
 
       });
     }
@@ -100,9 +100,7 @@ const app = {
       page.classList.toggle(classNames.nav.active, page.getAttribute('href') == pageId);
     }
 
-    console.log(pageId);
-
-    // window.location.hash = '#/' + pageId;
+    window.location.hash = '#/' + pageId;
 
   },
  
