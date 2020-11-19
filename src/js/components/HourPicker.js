97926c9adaa7c94824a0096ae4bdca7c85703743
Michał Górny <michalgorny94@gmail.com>
2020-11-19 21:08:50 +0100

    Add hourPicker class

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -12,24 +12,29 @@ export class hourPicker extends baseWidget {
     thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.hourPicker.output);
 
     thisWidget.initPlugin();
-    
+    thisWidget.value = thisWidget.dom.input;
   }
   parseValue(newValue){
     return utils.numberToHour(newValue);
   }
   isValid(newValue){
-    return isNaN(newValue);
+    return !isNaN(newValue);
   }
   renderValue(){
     const thisWidget = this;
 
-    thisWidget.dom.output = thisWidget.value;
+    thisWidget.dom.output.innerHTML = thisWidget.value;
+
+    console.log(thisWidget.value);
   }
   initPlugin(){
     const thisWidget = this;
 
+    // eslint-disable-next-line no-undef
+    rangeSlider.create(thisWidget.dom.input);
+
     thisWidget.dom.input.addEventListener('input', function() {
-      thisWidget.value = rangeSlider.create(thisWidget.dom.input);
+      thisWidget.value = thisWidget.dom.input;
     });
   }
 }
